125687aaabefc5f4d8ad46d28678c4fe0e15bc89
povle <pasha@blinov.co>
2023-10-25 00:39:48 +0300

    template book page

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,24 +1,26 @@
 function load_navbar(page_name) {
     $.get("/templates/navbar.html", function (value) {
         $('body').prepend(value);
-        $("#nav-"+page_name).addClass("active")
+        $("#nav-" + page_name).addClass("active")
     });
 };
 
 function render_books(books, template) {
-    $.get(template, function (tmpl_code) {
-        var tmpl = $.templates(tmpl_code);
-        books.forEach(element => {
-            var html = tmpl.render(element);
-            $("#cards").append(html);
+    $(document).ready(function () {
+        $.get(template, function (tmpl_code) {
+            var tmpl = $.templates(tmpl_code);
+            books.forEach(element => {
+                var html = tmpl.render(element);
+                $("#cards").append(html);
+            });
+
+            let script = document.createElement('script');
+            script.src = 'https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/js/star-rating.min.js';
+            document.head.appendChild(script);
+
+            let theme = document.createElement('script');
+            theme.src = 'https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/themes/krajee-svg/theme.js';
+            document.head.appendChild(theme);
         });
-
-        let script = document.createElement('script');
-        script.src = 'https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/js/star-rating.min.js';
-        document.head.appendChild(script);
-
-        let theme = document.createElement('script');
-        theme.src = 'https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/themes/krajee-svg/theme.js';
-        document.head.appendChild(theme);
     });
 };
