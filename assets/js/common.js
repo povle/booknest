b64f33a94bb815e46be46a4ef33301d226775dee
povle <pasha@blinov.co>
2023-10-25 01:44:12 +0300

    add local storage

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,6 +5,53 @@ function load_navbar(page_name) {
     });
 };
 
+function get_favorites() {
+    const raw = localStorage.getItem("favorites");
+    if (raw !== null) { return JSON.parse(raw); };
+    return [];
+};
+
+function is_favorite(id) {
+    return get_favorites().includes(id);
+};
+
+function toggle_favorites(id) {
+    var favorites = get_favorites();
+    if (favorites.contains(id)) {
+        favorites = favorites.filter(x => x!==id);
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+        return false;
+    };
+    favorites.push(id);
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+    return true;
+};
+
+function get_read_later() {
+    const raw = localStorage.getItem("read_later");
+    if (raw !== null) { return JSON.parse(raw); };
+    return [];
+};
+
+function is_read_later(id) {
+    return get_read_later().includes(id);
+};
+
+function toggle_read_later(id) {
+    var read_later = get_read_later();
+    if (read_later.contains(id)) {
+        read_later = read_later.filter(x => x !== id);
+        localStorage.setItem('read_later', JSON.stringify(read_later))
+        return false;
+    }
+    else {
+        read_later.push(id);
+        localStorage.setItem('read_later', JSON.stringify(read_later));
+        return true;
+    };
+};
+
+
 function render_books(books, template, callback=null) {
     $(document).ready(function () {
         $.get(template, function (tmpl_code) {
