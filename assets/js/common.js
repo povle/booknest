8d195f273d49141ecc9f05e675eb27eabcfaaa7b
povle <pasha@blinov.co>
2023-10-25 00:04:11 +0300

    common render function

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,21 @@
 $.get("/templates/navbar.html", function (value) {
     $('body').prepend(value);
 });
+
+function render_books(books, template) {
+    $.get(template, function (tmpl_code) {
+        var tmpl = $.templates(tmpl_code);
+        books.forEach(element => {
+            var html = tmpl.render(element);
+            $("#cards").append(html);
+        });
+
+        let script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/js/star-rating.min.js';
+        document.head.appendChild(script);
+
+        let theme = document.createElement('script');
+        theme.src = 'https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-star-rating@4.1.2/themes/krajee-svg/theme.js';
+        document.head.appendChild(theme);
+    });
+};
